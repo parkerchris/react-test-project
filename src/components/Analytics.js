b24664094f6ce48fd0806278190a08cbd3b6ff7a
Chris Parker <cwbparker@gmail.com>
2022-05-25 19:48:53 -0700

    update middle nav with analytics

---
+++ src/components/Analytics.js
@@ -0,0 +1,27 @@
+
+
+function Analytics() {
+    return (
+        <div className="analytics-container">
+            <div className="one">
+                <h3 className="analytics-header">VALUE</h3>
+                <p className="dollar">$4,240,000</p>
+                <div className="analytics-inner">
+                    <p className="analytics-p">+$230K</p>
+                    <p>+37%</p>
+                </div>
+            </div>
+
+            <div>
+                <h3 className="analytics-header">RENT ROLL</h3>
+                <p className="dollar">$17,000</p>
+                <div className="analytics-inner">
+                    <p className="analytics-p">+$1,300</p>
+                    <p>+40%</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
--- src/components/MiddleNav.js
+++ src/components/MiddleNav.js
@@ -1,11 +1,12 @@
 
-
+import Analytics from "./Analytics";
 
 function MiddleNav() {
     return (
       <div className="middle-nav-container">
           <div className="middle-nav-contains">
               <div className="main-cta"><p>Add a property</p></div>
+              <Analytics />
           </div>
       </div>
     );
